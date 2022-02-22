c7c2ca5bffac5e9523c0f697a19c96402a45bf6a
이 성 노 <kulbo0926@gmail.com>
2022-02-22 11:52:20 +0900

    별점 실제 표시  - Float(실수) vx Double(실수)  - Double값

--- app/src/main/java/com/neppplus/pizzaordert_20220222/adapters/StoreAdapter.kt
+++ app/src/main/java/com/neppplus/pizzaordert_20220222/adapters/StoreAdapter.kt
@@ -1,12 +1,18 @@
 package com.neppplus.pizzaordert_20220222.adapters
 
 import android.content.Context
+import android.media.Rating
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.neppplus.pizzaordert_20220222.R
 import com.neppplus.pizzaordert_20220222.datas.StoreData
+import com.willy.ratingbar.ScaleRatingBar
 
 class StoreAdapter(
     val mContext: Context,
@@ -20,6 +26,18 @@ class StoreAdapter(
             tempRow = LayoutInflater.from(mContext).inflate(R.layout.store_list_item, null)
         }
         val row = tempRow!!
+
+        val data = mList[position]
+
+        val imgLogo = row.findViewById<ImageView>(R.id.imgLogo)
+        val txtStoreName = row.findViewById<TextView>(R.id.txtStoreName)
+        val ratingBar = row.findViewById<ScaleRatingBar>(R.id.ratingBar)
+
+        txtStoreName.text = data.name
+
+        Glide.with(mContext).load(data.logoImageURL).into(imgLogo)
+
+        ratingBar.rating = data.rating.toFloat()
 
         return row
     }
