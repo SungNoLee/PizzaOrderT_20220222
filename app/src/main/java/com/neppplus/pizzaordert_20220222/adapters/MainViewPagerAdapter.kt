8c73f3c12407e66d28c835c5350e4b77c2630b39
이 성 노 <kulbo0926@gmail.com>
2022-02-22 09:55:12 +0900

    뷰페이저 어뎁터

---
+++ app/src/main/java/com/neppplus/pizzaordert_20220222/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,8 @@
+package com.neppplus.pizzaordert_20220222.adapters
+
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+}
