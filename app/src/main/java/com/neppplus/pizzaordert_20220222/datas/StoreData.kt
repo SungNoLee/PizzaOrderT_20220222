9f834caaeffe68687d1f889b46209444b9d18eac
이 성 노 <kulbo0926@gmail.com>
2022-02-22 10:51:20 +0900

    가게 데이터 표현용 클래스 추가

---
+++ app/src/main/java/com/neppplus/pizzaordert_20220222/datas/StoreData.kt
@@ -0,0 +1,10 @@
+package com.neppplus.pizzaordert_20220222.datas
+
+class StoreData(
+    val name: String,
+    val rating: Double,
+    val phoneNum: String,
+    val logoImageURL: String,
+
+    ) {
+}
