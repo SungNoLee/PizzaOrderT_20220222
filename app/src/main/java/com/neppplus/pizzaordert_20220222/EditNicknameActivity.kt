0d51ce06511eb43452a354340da01ed3fc84ab8e
이 성 노 <kulbo0926@gmail.com>
2022-02-22 16:00:06 +0900

    아직완성이 안됨.

---
+++ app/src/main/java/com/neppplus/pizzaordert_20220222/EditNicknameActivity.kt
@@ -0,0 +1,21 @@
+package com.neppplus.pizzaordert_20220222
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNicknameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nickname)
+
+    }
+
+    fun setupEvents() {
+
+    }
+
+    fun setValues() {
+
+    }
+
+}
